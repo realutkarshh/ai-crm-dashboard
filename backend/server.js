d7d6e8ba9ce9e6e1f279792c3a56efad6965d842
Utkarsh Singh <69565691+realutkarshh@users.noreply.github.com>
2026-07-15 21:58:18 +0530

    feat: analytics feature added

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ import noteRoutes from "./routes/note.routes.js";
 import taskRoutes from "./routes/task.routes.js";
 import contactRoutes from "./routes/contact.routes.js"
 import aiRoutes from "./routes/ai.routes.js";
+import analyticsRoutes from "./routes/analytics.routes.js";
 
 const app = express()
 
@@ -43,6 +44,7 @@ app.use("/api/notes", noteRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/contacts", contactRoutes);
 app.use("/api/ai",aiRoutes);
+app.use("/api/analytics",analyticsRoutes);
 
 app.use(notFound)
 app.use(errorHandler)
